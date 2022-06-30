383c4e615673cad4ddab55b7e04f306b773c5826
Adejuwon-00 <juwonmcalpha@gmail.com>
2022-06-30 10:25:03 +0100

    new commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import ThemeContextWrapper from './ThemeContextWrapper';
+
+ReactDOM.render(
+  <ThemeContextWrapper>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>{' '}
+  </ThemeContextWrapper>,
+  document.getElementById('root'),
+);
